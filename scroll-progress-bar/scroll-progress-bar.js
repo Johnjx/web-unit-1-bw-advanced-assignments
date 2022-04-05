815624e5a3ebe696f2d9e1debf1be0160f1dc1ee
John Ruddock <john.jxr@pm.me>
2022-04-05 00:11:25 -0400

    added HTML div boxes to give page room to scroll, added basic CSS focusing on styling the header which contains the progress bar divs, added JS function to fill the inner div progress bar based on the amount the user has scrolled

--- scroll-progress-bar/scroll-progress-bar.js
+++ scroll-progress-bar/scroll-progress-bar.js
@@ -1,3 +1,18 @@
 // most of the work here will be done with logic and a little math
 // what's the usual measurement of a progress bar? you'll need to measure the same way here
 // use that measurement to update the style of your inner fill element as you scroll
+
+// const scroll = document.getElementById("test");
+
+window.onscroll = function() {
+    scrollTest()
+};
+
+function scrollTest() {
+    const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let amountScrolled = (scroll/height) * 100;
+    document.getElementById("test").style.width = amountScrolled + "%";
+}
+
+
