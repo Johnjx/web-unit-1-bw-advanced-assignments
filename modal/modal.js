338f1aeeb90d5ea6a6815f26a68a0e50647e9688
John Ruddock <john.jxr@pm.me>
2022-04-04 21:48:37 -0400

    added HTML for page and a specific div for the modal HTML, added CSS focused on modal styling for the background, content and close button, added JS to change display property of modal window

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,23 @@
 // you will need to toggle the visibility off and on with DOM selectors
+
+const toggle = document.getElementById("modal-button");
+const modal = document.getElementById("modal-id");
+const span = document.querySelector('span');
+
+// toggle.addEventListener('click', function() {
+//     modal.classList
+// });
+
+toggle.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(Event) {
+    if (Event.target == modal) {
+        modal.style.display = "none";
+    }
+}
